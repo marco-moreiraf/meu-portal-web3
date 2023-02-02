8ebede9d41d95a0e1d2b92e962892a8f7f78ffc5
marco-moreiraf <marco.moreira94@gmail.com>
2023-02-01 21:31:24 -0300

    Create vue app and remove initial files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <div class="data-container">
+      <div class="header">
+        👋 Hello, World!
+      </div>
+
+      <div class="description">
+        Bora aprender sobre Web3! Conecta sua wallet e manda um salve pra nós!
+      </div>
+
+      <button class="wave-button" @click="wave">
+        Mandar salve 🌟
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    wave() {
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
